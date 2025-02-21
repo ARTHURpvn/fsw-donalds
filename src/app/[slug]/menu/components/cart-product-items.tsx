23944090daada656befe129d2f32ecbcf069c512
ARTHURpvn <arthursppavan@gmail.com>
2025-02-21 11:07:26 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/components/cart-product-items.tsx
+++ src/app/[slug]/menu/components/cart-product-items.tsx
@@ -1,40 +1,45 @@
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 
 interface CartProductProps {
   product: CartProduct;
 }
 
 const CartProductItems = ({ product }: CartProductProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
       {/* Esquerda */}
       <div className="flex items-center gap-3">
-
-      <div className="relative h-20 w-20 bg-gray-100 rounded-xl">
-        <Image src={product.imageUrl} alt={product.name} fill />
-      </div>
-
-      <div className="space-y-1">
-        <p className="max-w-[90%] truncate text-ellipsis text-xs">{product.name}</p>
-        <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
-
-        {/* Quantidade */}
-        <div className="flex items-center gap-1 text-center">
-          <Button variant={"outline"} className="h-7 w-7 rounded-lg">
-            <ChevronLeftIcon />
-          </Button>
-          <p className="text-xs w-7">{product.quantity}</p>
-          <Button variant={"destructive"} className="h-7 w-7 rounded-lg">
-            <ChevronRightIcon />
-          </Button>
+        <div className="relative h-20 w-20 rounded-xl bg-gray-100">
+          <Image src={product.imageUrl} alt={product.name} fill />
         </div>
-      </div>
+
+        <div className="space-y-1">
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
+
+          {/* Quantidade */}
+          <div className="flex items-center gap-1 text-center">
+            <Button variant={"outline"} className="h-7 w-7 rounded-lg" onClick={() => decreaseProductQuantity(product.id)}>
+              <ChevronLeftIcon />
+            </Button>
+            <p className="w-7 text-xs">{product.quantity}</p>
+            <Button variant={"destructive"} className="h-7 w-7 rounded-lg">
+              <ChevronRightIcon />
+            </Button>
+          </div>
+        </div>
       </div>
 
       <Button className="h-7 w-7 rounded-lg" variant={"outline"}>
