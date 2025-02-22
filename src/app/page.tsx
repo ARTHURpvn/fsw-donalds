386b2a4e2a9d56867d8b2ead0ce9a9d0f7d77458
ARTHURpvn <arthursppavan@gmail.com>
2025-02-22 12:57:49 -0300

    feat: add button in initial page for enter the mcDonalds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
 const HomePage = () => {
+  const router = useRouter();
+  const handleClickButton = () => router.push("/fsw-donalds");
   return (
-    <>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <Card>
+        <CardContent className="flex flex-col">
+          <CardHeader>
+            <h1 className="text-2xl font-bold">FSW DONALDS</h1>
+          </CardHeader>
+
+          <Button onClick={handleClickButton}>Entrar</Button>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
